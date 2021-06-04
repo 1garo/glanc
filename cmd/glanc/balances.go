5de39f98da665c419b77c0e82732c1f531e3ffe4
Alexandre Neto <alevardai427@gmail.com>
2021-06-04 19:27:24 -0300

    feat: create balance cli command

---
+++ cmd/glanc/balances.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"gitlab.com/1garo/glanc/database"
+)
+
+func balancesCmd() *cobra.Command {
+	var balancesCmd = &cobra.Command{
+		Use:   "balances",
+		Short: "Interact with balances (list...).",
+		// PreRunE: func(cmd *cobra.Command, args []string) error {
+		// 	return incorrectUsageErr()
+		// },
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	balancesCmd.AddCommand(balancesListCmd)
+
+	return balancesCmd
+}
+
+var balancesListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all balances.",
+	Run: func(cmd *cobra.Command, args []string) {
+		state, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer state.Close()
+
+		fmt.Println("Accounts balances:")
+		fmt.Println("__________________")
+		fmt.Println("")
+		for account, balance := range state.Balances {
+			fmt.Printf("%s: %d", account, balance)
+		}
+	},
+}
