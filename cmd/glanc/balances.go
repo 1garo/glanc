317d488da1171c90c9889a352c2735c27017730c
Alexandre Neto <alevardai427@gmail.com>
2021-06-09 00:29:56 -0300

    feat: refactor balances and document functions

--- cmd/glanc/balances.go
+++ cmd/glanc/balances.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// balancesCmd -> create balances cli command and it's configs
 func balancesCmd() *cobra.Command {
 	var balancesCmd = &cobra.Command{
 		Use:   "balances",
@@ -19,29 +20,32 @@ func balancesCmd() *cobra.Command {
 		},
 	}
 
-	balancesCmd.AddCommand(balancesListCmd)
+	balancesCmd.AddCommand(balancesListCmd())
 
 	return balancesCmd
 }
 
-var balancesListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "Lists all balances.",
-	Run: func(cmd *cobra.Command, args []string) {
-		state, err := database.NewStateFromDisk()
-		if err != nil {
-			fmt.Println(err)
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		defer state.Close()
+// balancesListCmd -> list all balances cmd
+func balancesListCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "list",
+		Short: "Lists all balances.",
+		Run: func(cmd *cobra.Command, args []string) {
+			state, err := database.NewStateFromDisk()
+			if err != nil {
+				fmt.Println(err)
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer state.Close()
 
-		fmt.Println("Accounts balances: ")
-		fmt.Println("__________________")
-		fmt.Println("")
+			fmt.Println("Accounts balances: ")
+			fmt.Println("__________________")
+			fmt.Println("")
 
-		for account, balance := range state.Balances {
-			fmt.Printf("%s: %d\n", account, balance)
-		}
-	},
+			for account, balance := range state.Balances {
+				fmt.Printf("%s: %d\n", account, balance)
+			}
+		},
+	}
 }
