7aaf835c8bcc0cab32467e8468c4070bd7a63b89
Alexandre Neto <alevardai427@gmail.com>
2021-06-09 00:31:26 -0300

    feat: refactor and document version function

--- cmd/glanc/version.go
+++ cmd/glanc/version.go
@@ -11,10 +11,13 @@ const Minor = "1"
 const Fix = "1"
 const Verbal = "TX Add && Balances List"
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Describes version.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Version: %s.%s.%s-beta %s", Major, Minor, Fix, Verbal)
-	},
+// versionCmd -> output cli version
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Describes version.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Version: %s.%s.%s-beta %s", Major, Minor, Fix, Verbal)
+		},
+	}
 }
